ed0c1bbcabc6d54cf39e05412cc1ed68f80aa7f6
Hyacinth13 <jpeckwriter@gmail.com>
2016-03-28 18:17:43 -0600

    resolved error when clicking on note title

--- routes/note.js
+++ routes/note.js
@@ -1,7 +1,13 @@
 var express = require('express');
+var mongoose = require('mongoose');
+var Note = mongoose.model('Note');
 var router = express.Router();
+
 //GET Note page
-router.get('/', function(req,res, next) {
-	res.render('note', {note:[], title: 'My Sticky Notes'});
+router.get('/:id', function(req,res, next) {
+	Note.findById(req.params.id, function(err,note) {
+		res.render('note', { note: note });
+	});
 });
+
 module.exports = router;
